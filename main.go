c15e7da7149435fdfa1b1621cb6783f8e9ef696a
josofm <jfurlanmachado@gmail.com>
2019-08-18 18:46:28 -0300

    using elephant database

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"github.com/lib/pq"
+	"github.com/subosito/gotenv"
 	"log"
 	"net/http"
-	"strconv"
+	"os"
+	//"strconv"
 )
 
 type Deck struct {
@@ -15,18 +19,30 @@ type Deck struct {
 }
 
 var decks []Deck
+var db *sql.DB
+
+func init() {
+	gotenv.Load()
+}
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
+	pgUrl, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
+	logFatal(err)
 
-	decks = append(decks,
-		Deck{ID: 1, Commander: "Scarab-God", Owner: "Mirko"},
-		Deck{ID: 2, Commander: "Borborigmo", Owner: "Joso"},
-		Deck{ID: 3, Commander: "Saskia", Owner: "Jesus"},
-		Deck{ID: 4, Commander: "Atraxa", Owner: "Graia"},
-		Deck{ID: 5, Commander: "Muldrotha", Owner: "Jonatan"},
-	)
+	db, err = sql.Open("postgres", pgUrl)
+	logFatal(err)
+
+	err = db.Ping()
+	logFatal(err)
 
 	router := mux.NewRouter()
+
 	router.HandleFunc("/decks", getDecks).Methods("GET")
 	router.HandleFunc("/decks/{id}", getDeck).Methods("GET")
 	router.HandleFunc("/decks", addDeck).Methods("POST")
@@ -37,67 +53,79 @@ func main() {
 }
 
 func getDecks(w http.ResponseWriter, r *http.Request) {
+	var deck Deck
+	decks = []Deck{}
+
+	rows, err := db.Query("select * from decks")
+	logFatal(err)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&deck.ID, &deck.Commander, &deck.Owner)
+		logFatal(err)
+
+		decks = append(decks, deck)
+	}
+
 	json.NewEncoder(w).Encode(decks)
+
 	log.Println("Get decks is called")
 }
 
 func getDeck(w http.ResponseWriter, r *http.Request) {
+	var deck Deck
 	params := mux.Vars(r)
 
-	idInteger, err := strconv.Atoi(params["id"])
-	if err != nil {
-		log.Fatalln("Could not convert to int")
-	}
+	rows := db.QueryRow("select * from decks where id=$1", params["id"])
+	err := rows.Scan(&deck.ID, &deck.Commander, &deck.Owner)
+	logFatal(err)
 
-	for _, deck := range decks {
-		if deck.ID == idInteger {
-			json.NewEncoder(w).Encode(&deck)
-		}
-	}
+	json.NewEncoder(w).Encode(deck)
+
 	log.Println("Get deck is called")
 }
 
 func addDeck(w http.ResponseWriter, r *http.Request) {
 	var deck Deck
+	var deckID int
 
-	_ = json.NewDecoder(r.Body).Decode(&deck)
+	json.NewDecoder(r.Body).Decode(&deck)
 
-	decks = append(decks, deck)
+	err := db.QueryRow("insert into decks (owner, commander) values($1, $2) RETURNING id;",
+		deck.Owner, deck.Commander).Scan(&deckID)
+	logFatal(err)
 
-	json.NewEncoder(w).Encode(decks)
+	json.NewEncoder(w).Encode(deckID)
 
 	log.Println("Add deck is called")
 }
 
 func updateDeck(w http.ResponseWriter, r *http.Request) {
 	var deck Deck
-	_ = json.NewDecoder(r.Body).Decode(&deck)
+	json.NewDecoder(r.Body).Decode(&deck)
 
-	for i, item := range decks {
-		if item.ID == deck.ID {
-			decks[i] = deck
-		}
-	}
+	result, err := db.Exec("update decks set owner=$1, commander=$2 where id=$3 RETURNING id;",
+		&deck.Owner, &deck.Commander, &deck.ID)
+	logFatal(err)
 
-	json.NewEncoder(w).Encode(decks)
+	rowsUpdated, err := result.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsUpdated)
 
 	log.Println("Update deck is called")
 }
 
 func removeDeck(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
+	result, err := db.Exec("delete from decks where id = $1", params["id"])
+	logFatal(err)
 
-	idInteger, err := strconv.Atoi(params["id"])
-	if err != nil {
-		log.Fatalln("Could not convert to int")
-	}
+	rowsDeleted, err := result.RowsAffected()
+	logFatal(err)
 
-	for i, item := range decks {
-		if item.ID == idInteger {
-			decks = append(decks[:i], decks[i+1:]...)
-		}
-	}
-	json.NewEncoder(w).Encode(decks)
+	json.NewEncoder(w).Encode(rowsDeleted)
 
 	log.Println("Delete deck is called")
 }
